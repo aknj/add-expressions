257015cd2a8beb8074765904c7a4a751f299fbdf
aknj <aknjn@protonmail.com>
2020-07-16 15:10:46 +0200

    add new test cases and fix code to make them pass; some refactoring and comment

--- add-expressions.js
+++ add-expressions.js
@@ -1,16 +1,30 @@
-const addExpressions = (exp1, exp2) => {
+const addExpressions = (exprA, exprB) => {
+  // expressions are stored in Map objects, where each term is represented by
+  // an exponent => coefficient pair
+  //
+  // Ax^a + Bx^b + ...
+  // Map { a => A, b => B, ... }
+  // for example: 4x^3 + 3x^2 + 3
+  // Map { 3 => 4, 2 => 3, 0 => 3 }
+
   let output = new Map();
-  for (let [exponent, coefficient] of exp1) {
-    if (exp2.has(exponent)) {
-      output.set(exponent, coefficient + exp2.get(exponent));
-      exp2.delete(exponent);
+
+  for (let [exponent, coefficientA] of exprA) {
+    if (exprB.has(exponent)) {
+      const sumOfCoefficients = coefficientA + exprB.get(exponent);
+
+      if (sumOfCoefficients !== 0) {
+        output.set(exponent, sumOfCoefficients);
+      }
+
+      exprB.delete(exponent);
     }
     else {
-      output.set(exponent, coefficient);
+      output.set(exponent, coefficientA);
     }
   }
 
-  for (let [exponent, coefficient] of exp2) {
+  for (let [exponent, coefficient] of exprB) {
     output.set(exponent, coefficient);
   }
 
--- add-expressions.test.js
+++ add-expressions.test.js
@@ -1,15 +1,37 @@
 const addExpressions = require('./add-expressions');
 
 test('polynomials from Example', () => {
-  const exp1 = new Map([[2, 2], [0, 3]]);
+  const exp1 = new Map([        [2, 2], [0, 3]]);
   const exp2 = new Map([[3, 3], [2, 1]]);
   const expectedOutput = new Map([[3, 3], [2, 3], [0, 3]]);
   expect(addExpressions(exp1, exp2)).toEqual(expectedOutput);
 });
 
-test('the same expressions', () => {
+test('the same expression twice', () => {
   const exp1 = new Map([[2, 2], [0, 3]]);
   const exp2 = new Map([[2, 2], [0, 3]]);
   const expectedOutput = new Map([[2, 4], [0, 6]]);
+  expect(addExpressions(exp1, exp2)).toEqual(expectedOutput);
+});
+
+test('some real exponents', () => {
+  const exp1 = new Map([          [2, 2], [1.1, 3]]);
+  const exp2 = new Map([[2.0005, 2],      [1.1, 3]]);
+  const expectedOutput = new Map([[2.0005, 2], [2, 2], [1.1, 6]]);
+  expect(addExpressions(exp1, exp2)).toEqual(expectedOutput);
+});
+
+test('some real coefficients', () => {
+  const exp1 = new Map([          [2, 2],  [1, 0.3], [0, 3]]);
+  const exp2 = new Map([[3, 2.1], [2, 1.5]]);
+  const expectedOutput = new Map([[3, 2.1], [2, 3.5], [1, 0.3], [0, 3]]);
+  expect(addExpressions(exp1, exp2)).toEqual(expectedOutput);
+});
+
+
+test('some terms cancel each other out', () => {
+  const exp1 = new Map([[3, 2],  [2, 0.5], [1, 2]]);
+  const exp2 = new Map([[3, -2], [2, 1]]);
+  const expectedOutput = new Map([[2, 1.5], [1, 2]]);
   expect(addExpressions(exp1, exp2)).toEqual(expectedOutput);
 });
